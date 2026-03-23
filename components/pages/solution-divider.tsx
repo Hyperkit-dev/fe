4724ec6237113c11428a4f4631946cecd42c7e47
JustineDevs <TraderGOfficial@gmail.com>
2026-03-23 16:03:26 +0800

    feat: enhance solution-divider component

--- components/pages/solution-divider.tsx
+++ components/pages/solution-divider.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
 
 export const Divider: React.FC = () => (
-  <div className="w-full max-w-2xl h-px bg-gradient-to-r from-transparent via-white/10 to-transparent my-12" />
+  <div className="w-full max-w-2xl h-px bg-gradient-to-r from-transparent via-white/10 to-transparent my-8 md:my-10" />
 );
