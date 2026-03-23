afbe874cf951d0ee5f85ea608e9ececc0406941e
JustineDevs <TraderGOfficial@gmail.com>
2026-03-23 16:09:23 +0800

    remove: delete components/defi/index.ts

--- components/defi/index.ts
+++
@@ -1,8 +0,0 @@
-// Export all DeFi components
-export { ConnectWallet, default as ConnectWalletDefault } from './ConnectWallet';
-export { Swap, default as SwapDefault } from './Swap';
-export { Bridge, default as BridgeDefault } from './Bridge';
-export { Staking, default as StakingDefault } from './Staking';
-export { Faucet, default as FaucetDefault } from './Faucet';
-export { NetworkSelector, default as NetworkSelectorDefault } from './NetworkSelector';
-
